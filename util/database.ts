4e1ab025f076b866f61466aee404b362bdd3f5a4
katharina jäger <jaeger.kt@gmail.com>
2022-04-17 00:45:21 +0200

    Create database file

---
+++ util/database.ts
@@ -0,0 +1,57 @@
+import camelcaseKeys from 'camelcase-keys';
+import { config } from 'dotenv-safe';
+import postgres from 'postgres';
+import { User } from './types';
+
+config();
+
+// Type needed for the connection function below
+declare module globalThis {
+  let postgresSqlClient: ReturnType<typeof postgres> | undefined;
+}
+
+// Connect only once to the database
+// https://github.com/vercel/next.js/issues/7811#issuecomment-715259370
+function connectOneTimeToDatabase() {
+  let sql;
+
+  if (process.env.NODE_ENV === 'production' && process.env.DATABASE_URL) {
+    sql = postgres();
+    // Heroku needs SSL connections but
+    // has an "unauthorized" certificate
+    // https://devcenter.heroku.com/changelog-items/852
+    sql = postgres({ ssl: { rejectUnauthorized: false } });
+  } else {
+    if (!globalThis.postgresSqlClient) {
+      globalThis.postgresSqlClient = postgres();
+    }
+    sql = globalThis.postgresSqlClient;
+  }
+  return sql;
+}
+
+// Connect to PostgreSQL
+const sql = connectOneTimeToDatabase();
+
+export async function createUser(username: string, passwordHash: string) {
+  const [user] = await sql<[User]>`
+    INSERT INTO users
+      (username, password_hash)
+    VALUES
+      (${username}, ${passwordHash})
+    RETURNING
+      id,
+      username
+  `;
+  return camelcaseKeys(user);
+}
+
+export async function getUserByUsername(username: string) {
+  const [user] = await sql<[User | undefined]>`
+    SELECT
+      id
+    FROM users
+    WHERE username = ${username}
+  `;
+  return user && camelcaseKeys(user);
+}
