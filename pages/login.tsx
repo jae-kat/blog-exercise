00ee64ffbf420a94eb6305c46d563e46713b70b3
katharina jäger <jaeger.kt@gmail.com>
2022-04-17 00:51:29 +0200

    Use controlled components

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,25 +1,37 @@
 import Head from 'next/head';
+import { useState } from 'react';
 
 export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <>
       <Head>
         <title>Login</title>
         <meta name="description" content="Blog login" />
       </Head>
-      <div>
+      <form>
         <h1>Login</h1>
         <div>
           <label>
             Username
-            <input />
+            <input
+              value={username}
+              onChange={(event) => setUsername(event.currentTarget.value)}
+            />
           </label>
           <label>
             Password
-            <input type="password" />
+            <input
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.currentTarget.value)}
+            />
           </label>
+          <button>Login</button>
         </div>
-      </div>
+      </form>
     </>
   );
 }
