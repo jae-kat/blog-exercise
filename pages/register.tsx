0a961fd7dcaa50beee924a7120fa43628d0e0d9f
katharina jäger <jaeger.kt@gmail.com>
2022-04-17 00:47:47 +0200

    Write form, register function, add tailwind styles

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,25 +1,53 @@
 import Head from 'next/head';
+import { useState } from 'react';
 
 export default function Register() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <>
       <Head>
         <title>Register</title>
-        <meta name="description" content="Blog login" />
+        <meta name="description" content="Blog user registration" />
       </Head>
-      <div>
-        <h1>Register</h1>
-        <div>
+      <form
+        className="flex flex-col items-center justify-center h-screen gap-2"
+        onSubmit={async (event) => {
+          event.preventDefault();
+          const registerResponse = await fetch('/api/register', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username: username, password: password }),
+          });
+        }}
+      >
+        <h1 className="text-2xl font-bold my-3">Register</h1>
+        <div className="flex flex-col max-w-max gap-2">
           <label>
             Username
-            <input />
+            <br />
+            <input
+              className="rounded border-2  border-solid"
+              value={username}
+              onChange={(event) => setUsername(event.currentTarget.value)}
+            />
           </label>
           <label>
             Password
-            <input type="password" />
+            <br />
+            <input
+              className="rounded border-2 border-solid"
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.currentTarget.value)}
+            />
           </label>
+          <button className="rounded-full p-2 my-3 text-purple-50 bg-purple-800 border-white border-solid hover:border-purple-300 border-2">
+            Register
+          </button>
         </div>
-      </div>
+      </form>
     </>
   );
 }
